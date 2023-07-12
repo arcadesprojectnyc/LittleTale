2aaca8f90ebdbe5085bded8963b60b22272e25f4
Yang Yang <yang.yang.poly@gmail.com>
2023-07-11 21:58:38 -0400

    add ui and api integration for hint

--- src/components/WriteStory.jsx
+++ src/components/WriteStory.jsx
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { handleAPIRequest, handleMessageRole } from "../utils/GPTUtils";
 import { UserContext } from "./UserContext";
+import hint from "../images/hint.png";
 
 function WriteStory() {
   const { token, char_type, char_name, where_is_char, setWriteStoryMsgCxt } =
@@ -9,6 +10,8 @@ function WriteStory() {
   const [inputText, setInputText] = useState("");
   const messageContainerRef = useRef(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [hintMode, setHintMode] = useState(false);
+  const [hintText, setHintText] = useState([]);
   const [messages, setMessages] = useState([]);
   const navigate = useNavigate();
 
@@ -60,6 +63,8 @@ function WriteStory() {
   const consistent_requirement = "Continue the story user wrote.";
   const beginnings =
     "A " + char_type + " named " + char_name + " was " + where_is_char + ".";
+  const hint_prompt =
+    "Give me 3 directions with 1 simple sentence I can potentially continue this story.";
 
   const system_prompt =
     prompt_settings +
@@ -82,7 +87,7 @@ function WriteStory() {
     setIsLoading(true);
     const apiResponse = await handleAPIRequest(token, msgs);
     setIsLoading(false);
-    if (apiResponse !== "") {
+    if (apiResponse !== null) {
       msgs = [...msgs, apiResponse];
     }
     setMessages(msgs);
@@ -96,6 +101,33 @@ function WriteStory() {
     console.log("[WriteStory] Finish Story");
     setWriteStoryMsgCxt(messages);
     navigate("/review-story/");
+  };
+
+  const handleHintClick = async () => {
+    console.log("[WriteStory]Image clicked");
+    setHintMode(true);
+    let msgs = messages;
+    if (msgs.length != 0) {
+      const userRoleMessage = {
+        role: "user",
+        content: hint_prompt,
+      };
+      msgs = [...msgs, userRoleMessage];
+    }
+    setIsLoading(true);
+    const apiResponse = await handleAPIRequest(token, msgs);
+    setIsLoading(false);
+    if (apiResponse !== null) {
+      let hintText = ["Here are 3 suggestions for you: "];
+      hintText = [...hintText, ...apiResponse.content.split("\n")];
+      setHintText(hintText);
+      console.log("got hint: ", apiResponse.content);
+    }
+  };
+
+  const handleClosePopup = () => {
+    setHintMode(false);
+    setHintText([]);
   };
 
   const filteredMessages = messages.filter(
@@ -148,6 +180,50 @@ function WriteStory() {
           onChange={handleInputChange}
           disabled={isLoading}
         ></textarea>
+        <img
+          src={hint}
+          alt="hint"
+          onClick={isLoading ? null : handleHintClick}
+          style={{
+            width: "40px",
+            height: "60px",
+            padding: "50px",
+            cursor: isLoading ? "not-allowed" : "pointer",
+          }}
+        />
+        {hintMode && (
+          <div
+            style={{
+              position: "absolute",
+              top: "40px",
+              left: "50%",
+              transform: "translateX(-50%)",
+              background: "#0c48c0",
+              padding: "10px",
+              boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+              zIndex: 1,
+            }}
+          >
+            <button
+              style={{
+                position: "absolute",
+                top: "45px",
+                right: "30px",
+                cursor: "pointer",
+                border: "solid",
+                background: "#ffffff",
+              }}
+              onClick={handleClosePopup}
+            >
+              X
+            </button>
+            {hintText.map((message, index) => (
+              <p key={index} style={{ textAlign: "left" }}>
+                {message}
+              </p>
+            ))}
+          </div>
+        )}
       </div>
       <div style={{ marginTop: "20px" }}>
         <button onClick={handleContinueStoryClick} disabled={isLoading}>
