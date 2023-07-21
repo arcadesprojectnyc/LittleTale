89c475888ab94b7b189e459596d053f7c1074ed1
Yang Yang <yang.yang.poly@gmail.com>
2023-07-21 13:06:51 -0400

    Revisit the story setting page to add beginning page

--- src/components/StorySetting.jsx
+++ src/components/StorySetting.jsx
@@ -1,56 +1,61 @@
-import React, {useState, useContext} from 'react';
-import { useNavigate } from 'react-router-dom';
-import { UserContext } from './UserContext';
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 function StorySetting() {
-    const [charactor_type, setCharactorType] = useState('');
-    const [charactor_name, setCharactorName] = useState('');
-    const [where_is_charactor, setWhereIsCharactor] = useState('');
-    const navigate = useNavigate();
-    const { token, setCharTypeCxt, setCharNameCxt, setWhereIsCharCxt } = useContext(UserContext);
+  const [charactor_name, setCharactorName] = useState("");
+  const [beginning, setBeginning] = useState("");
+  const navigate = useNavigate();
+  const { setCharNameCxt, setBeginningCxt } = useContext(UserContext);
 
-    const handleCharactorTypeChange = (event) => {
-        setCharactorType(event.target.value);
+  const handleCharactorNameChange = (event) => {
+    setCharactorName(event.target.value);
+  };
+
+  const handleBeginningChange = (event) => {
+    setBeginning(event.target.value);
+  };
+  const handleButtonClick = () => {
+    if (charactor_name == "" || beginning == "") {
+      console.log(
+        "[Select.log] char_name: " +
+          charactor_name +
+          ", beginning: " +
+          beginning
+      );
+      alert(`Let's write the charactor name and the beginning!`);
+    } else {
+      setCharNameCxt(charactor_name);
+      setBeginningCxt(beginning);
+      let path = "/write-story/";
+      navigate(path);
     }
+  };
 
-    const handleCharactorNameChange = (event) => {
-        setCharactorName(event.target.value);
-    }
-
-    const handleWhereIsCharactorChange = (event) => {
-        setWhereIsCharactor(event.target.value);
-    }
-
-    const handleButtonClick = () => {
-        if (charactor_type == "" ||
-            charactor_name == "" ||
-            where_is_charactor == "") {
-            console.log("[Select.log] char_type: " + charactor_type +
-                ", char_name: " + charactor_name +
-                ", where: " + where_is_charactor)
-            alert(`Let's write the charactor, name and background first!`);
-        } else {
-            setCharTypeCxt(charactor_type);
-            setCharNameCxt(charactor_name);
-            setWhereIsCharCxt(where_is_charactor);
-            let path = "/write-story/";
-            navigate(path);
-        }
-    }
-
-    return (
-        <div>
-            <p style={{ fontSize: '20px' }}>What's the character?</p>
-            <input type="text" value={charactor_type} onChange={handleCharactorTypeChange} />
-            <p style={{ fontSize: '20px' }}>What's the character's name?</p>
-            <input type="text" value={charactor_name} onChange={handleCharactorNameChange} />
-            <p style={{ fontSize: '20px' }}>Where is the character?</p>
-            <input type="text" value={where_is_charactor} onChange={handleWhereIsCharactorChange} />
-            <div>
-                <button onClick={handleButtonClick}>Start Writing!!!</button>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <p style={{ fontSize: "20px" }}>Name your character:</p>
+      <input
+        type="text"
+        value={charactor_name}
+        onChange={handleCharactorNameChange}
+      />
+      <p style={{ fontSize: "20px" }}>Write the beginning of the Story:</p>
+      <textarea
+        style={{
+          border: "1px solid #ccc",
+          height: "20vh",
+          padding: "10px",
+          width: "60vh",
+        }}
+        value={beginning}
+        onChange={handleBeginningChange}
+      ></textarea>
+      <div>
+        <button onClick={handleButtonClick}>Start Writing!!!</button>
+      </div>
+    </div>
+  );
 }
 
 export default StorySetting;
--- src/components/UserContext.js
+++ src/components/UserContext.js
@@ -6,25 +6,20 @@ export const UserContext = createContext();
 // Create a provider component
 export function UserProvider({ children }) {
   const [token, setToken] = useState(null);
-  const [char_type, setCharType] = useState(null);
   const [char_name, setCharName] = useState(null);
-  const [where_is_char, setWhereIsChar] = useState(null);
+  const [beginning, setBeginning] = useState(null);
   const [write_story_msgs, setWriteStoryMsgs] = useState(null);
 
   const setTokenValue = (token) => {
     setToken(token);
   };
 
-  const setCharTypeValue = (char_type) => {
-    setCharType(char_type);
-  };
-
   const setCharNameValue = (char_name) => {
     setCharName(char_name);
   };
 
-  const setWhereIsCharValue = (where_is_char) => {
-    setWhereIsChar(where_is_char);
+  const setBeginningValue = (beginning) => {
+    setBeginning(beginning);
   };
 
   const setWriteStoryMsgsValue = (write_story_msgs) => {
@@ -35,12 +30,10 @@ export function UserProvider({ children }) {
   const value = {
     token,
     setTokenCxt: setTokenValue,
-    char_type,
-    setCharTypeCxt: setCharTypeValue,
     char_name,
     setCharNameCxt: setCharNameValue,
-    where_is_char,
-    setWhereIsCharCxt: setWhereIsCharValue,
+    beginning,
+    setBeginningCxt: setBeginningValue,
     write_story_msgs,
     setWriteStoryMsgCxt: setWriteStoryMsgsValue,
   };
