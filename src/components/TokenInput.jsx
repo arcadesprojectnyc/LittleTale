b1dc3fa18a5a9abbb39f8611267c12a4c7279851
yma415 <yma415@gatech.edu>
2023-07-21 13:01:46 -0400

    update name and theme

--- src/components/TokenInput.jsx
+++ src/components/TokenInput.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
-import logo from "../images/logo_new.png";
 import "../App.css";
 import { UserContext } from "./UserContext";
 
