5d1e0a7a37b21ef6347b3f6ef845320e3381ec47
yma415 <yma415@gatech.edu>
2023-07-21 16:36:54 -0400

    solve conflict

--- src/components/StorySetting.jsx
+++ src/components/StorySetting.jsx
@@ -1,65 +1,54 @@
-import React, {useState, useContext} from 'react';
-import { useNavigate } from 'react-router-dom';
-import { UserContext } from './UserContext';
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 function StorySetting() {
-    const [charactor_type, setCharactorType] = useState('');
-    const [charactor_name, setCharactorName] = useState('');
-    const [where_is_charactor, setWhereIsCharactor] = useState('');
-    const navigate = useNavigate();
-    const { token, setCharTypeCxt, setCharNameCxt, setWhereIsCharCxt } = useContext(UserContext);
+  const [charactor_name, setCharactorName] = useState("");
+  const [beginning, setBeginning] = useState("");
+  const navigate = useNavigate();
+  const { setCharNameCxt, setBeginningCxt } = useContext(UserContext);
 
-    const handleCharactorTypeChange = (event) => {
-        setCharactorType(event.target.value);
+  const handleCharactorNameChange = (event) => {
+    setCharactorName(event.target.value);
+  };
+
+  const handleBeginningChange = (event) => {
+    setBeginning(event.target.value);
+  };
+  const handleButtonClick = () => {
+    if (charactor_name == "" || beginning == "") {
+      console.log(
+        "[Select.log] char_name: " +
+          charactor_name +
+          ", beginning: " +
+          beginning
+      );
+      alert(`Let's write the charactor name and the beginning!`);
+    } else {
+      setCharNameCxt(charactor_name);
+      setBeginningCxt(beginning);
+      let path = "/write-story/";
+      navigate(path);
     }
-
-    const handleCharactorNameChange = (event) => {
-        setCharactorName(event.target.value);
-    }
-
-    const handleWhereIsCharactorChange = (event) => {
-        setWhereIsCharactor(event.target.value);
-    }
-
-    const handleButtonClick = () => {
-        if (charactor_type == "" ||
-            charactor_name == "" ||
-            where_is_charactor == "") {
-            console.log("[Select.log] char_type: " + charactor_type +
-                ", char_name: " + charactor_name +
-                ", where: " + where_is_charactor)
-            alert(`Let's write the charactor, name and background first!`);
-        } else {
-            setCharTypeCxt(charactor_type);
-            setCharNameCxt(charactor_name);
-            setWhereIsCharCxt(where_is_charactor);
-            let path = "/write-story/";
-            navigate(path);
-        }
-    }
-
+  };
+  
     return (
         <div className="setting-background-container">
             <div className="centered-content">
             <p className="interface-header">What's in your mind?</p>
-            <p className="interface-text"> What's the character?</p>
-            <input type="text" 
-            value={charactor_type} 
-            onChange={handleCharactorTypeChange} 
-            className="input-field"
-            />
-            <p className="interface-text">What's the character's name?</p>
+            <p className="interface-text">Name your character:</p>
             <input type="text" 
             value={charactor_name} 
             onChange={handleCharactorNameChange}
             className="input-field"
              />
-            <p className="interface-text">Where is the character?</p>
-            <input type="text" 
-            value={where_is_charactor} 
-            onChange={handleWhereIsCharactorChange}
-            className="input-field"
-             />
+            <p className="interface-text">Write the beginning of the Story:</p>
+            <textarea
+                className="input-textarea"
+                value={beginning}
+                onChange={handleBeginningChange}
+                placeholder="Write the beginning of the Story..."
+              ></textarea>
             <div>
              < button onClick={handleButtonClick} className="button">
              I'm ready!
